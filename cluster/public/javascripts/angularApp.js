e7b234b68dcbdb06e14b1bface41d9b2797941ce
EC2 Default User <ec2-user@ip-172-31-38-240.us-west-2.compute.internal>
2015-04-14 15:36:49 +0000

    lab 11

---
+++ cluster/public/javascripts/angularApp.js
@@ -0,0 +1,18 @@
+angular.module('clusterApp', [])
+.controller('MainCtrl', [
+'$scope', '$http',
+function($scope, $http){
+  $scope.cluster = [{pid:1234}];
+  $scope.getPIDs = function(){
+     $scope.cluster= [{pid:12},{pid:34}];
+  };
+  $scope.getMyPIDs = function(){
+      for(i=0;i<100;i++)
+      {
+      $http.get('/pid').success(function(data){
+	 $scope.cluster.push(data);
+      });
+      }
+}
+}
+]);
